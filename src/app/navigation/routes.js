05f42592d459d88108d059a1605a07cea9f0ae55
Ryan C. Collins <rccproductions@mac.com>
2019-12-15 21:30:32 -0500

    Add icons

--- src/app/navigation/routes.js
+++ src/app/navigation/routes.js
@@ -1,14 +1,43 @@
+import React from 'react';
+import { FontAwesome, MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 import { Discover, Chat, User } from "../screens";
 
 export const routeConfig = {
   Discover: {
-    screen: Discover
+    screen: Discover,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <MaterialCommunityIcons
+          color={focused ? 'blue' : 'black'}
+          size={20}
+          name="telescope"
+        />
+      ),
+    }
   },
   Chat: {
-    screen: Chat
+    screen: Chat,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <Entypo
+          color={focused ? 'blue' : 'black'}
+          size={20}
+          name="chat"
+        />
+      ),
+    }
   },
   User: {
-    screen: User
+    screen: User,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <FontAwesome
+          color={focused ? 'blue' : 'black'}
+          size={20}
+          name="user"
+        />
+      ),
+    }
   }
 };
 
