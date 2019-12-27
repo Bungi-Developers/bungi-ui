ee872c3db8bbbd1aa7f4f88f11d0e634a127b125
Ryan C. Collins <rccproductions@mac.com>
2019-12-27 13:54:37 -0500

    Add editable state to user profile

--- src/app/screens/User/index.js
+++ src/app/screens/User/index.js
@@ -7,22 +7,18 @@ import USER_QUERY from "./query";
 import UserProfile from "../../templates/UserProfile";
 
 const User = () => {
-  const {
-    loading,
-    error,
-    data: { user }
-  } = useQuery(USER_QUERY);
+  const { loading, error, data } = useQuery(USER_QUERY);
 
   // TODO: add loading and error components
   if (loading) {
     return <Text>Loading</Text>;
   }
-  if (error || !user) {
+  if (error || !data.user) {
     return (
       <Text>{(error && error.message) || "An unknown error occurred"}</Text>
     );
   }
-  return <UserProfile user={user} />;
+  return <UserProfile editable={true} user={data.user} />;
 };
 
 export default compose(withSafeScroll)(User);
--- src/app/templates/UserProfile/index.js
+++ src/app/templates/UserProfile/index.js
@@ -1,15 +1,36 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, Button } from "react-native";
 import { compose } from "redux";
 import { withSafeScroll, withStyles } from "../../HOCs";
-import { StyledImage } from "../../components";
+import { StyledImage, ContentBox } from "../../components";
 import FeaturedPane from "./FeaturedPane";
 import InfoPane from "./InfoPane";
 
-const UserProfile = ({ user: { name, rating, profile }, styles }) => {
+const UserProfile = ({
+  user: { name, rating, profile },
+  editable = false,
+  styles
+}) => {
   const [featuredImage, ...imageUrls] = profile.imageUrls;
+  const [editing, setEditing] = useState(false);
+  const toggleEditing = () => setEditing(!editing);
   return (
     <View style={styles.container}>
+      {editable && (
+        <ContentBox>
+          <View
+            style={{ ...styles.toolbar, ...(editing ? styles.editing : {}) }}
+          >
+            {editing && (
+              <>
+                <Button title="Cancel" onPress={toggleEditing} />
+                <Button title="Save" onPress={toggleEditing} />
+              </>
+            )}
+            {!editing && <Button title="Edit" onPress={toggleEditing} />}
+          </View>
+        </ContentBox>
+      )}
       <FeaturedPane name={name} rating={rating} url={featuredImage} />
       <InfoPane {...profile} />
       {imageUrls.map((url, i) => (
@@ -24,6 +45,17 @@ export default compose(
   withStyles({
     container: {
       flex: 1
+    },
+    toolbar: {
+      width: "100%",
+      height: 45,
+      display: "flex",
+      flexDirection: "row",
+      marginRight: 12,
+      justifyContent: "flex-end"
+    },
+    editing: {
+      justifyContent: "space-between"
     }
   })
 )(UserProfile);
