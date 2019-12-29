9d2df041fc4c6fa4efd8bd9779df57e5fc676c7a
Ryan C. Collins <rccproductions@mac.com>
2019-12-28 20:24:07 -0500

    Fix linting error

--- src/app/screens/Chat/ChatTable/index.js
+++ src/app/screens/Chat/ChatTable/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import uuid from "lodash/uniqueId";
 import { compose } from "redux";
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 import { withStyles } from "../../../HOCs";
 import ChatTableItem from "./ChatTableItem";
 
