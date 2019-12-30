a1837a5588ba0155740fdcdb126d430979bc98ed
Ryan C. Collins <rccproductions@mac.com>
2019-12-29 19:43:17 -0500

    Disable scrolling of infopane

--- src/app/templates/UserProfile/InfoPane/index.js
+++ src/app/templates/UserProfile/InfoPane/index.js
@@ -24,6 +24,7 @@ const InfoPane = ({
 }) => (
   <ContentBox border={true}>
     <FlatList
+      scrollEnabled={false}
       renderItem={({ item }) => <InfoPaneItem {...item} />}
       keyExtractor={item => item.id}
       data={[
