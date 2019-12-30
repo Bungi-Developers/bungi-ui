4bec285d967e4bb48cabc6bb85cf3fb3a3b80772
Ryan C. Collins <rccproductions@mac.com>
2019-12-29 19:51:50 -0500

    Add scroll handler when dismissing users

--- src/app/templates/UserProfile/Content/index.js
+++ src/app/templates/UserProfile/Content/index.js
@@ -1,21 +1,30 @@
-import React from "react";
-import { View } from "react-native";
-import { compose } from "redux";
-import { withSafeScroll } from "../../../HOCs";
+import React, { useRef, useEffect } from "react";
+import { View, ScrollView } from "react-native";
 import { StyledImage } from "../../../components";
 import InfoPane from "../InfoPane";
 
 const UserProfileContent = ({ profile }) => {
   const [featuredImage, ...imageUrls] = profile.imageUrls;
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current.scrollTo({
+      x: 0,
+      y: 0,
+      animated: true
+    });
+  }, [profile]);
   return (
-    <View>
-      <StyledImage url={featuredImage} />
-      <InfoPane {...profile} />
-      {imageUrls.map((url, i) => (
-        <StyledImage key={i} url={url} />
-      ))}
-    </View>
+    <ScrollView ref={ref}>
+      <View>
+        <StyledImage url={featuredImage} />
+        <InfoPane {...profile} />
+        {imageUrls.map((url, i) => (
+          <StyledImage key={i} url={url} />
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
-export default compose(withSafeScroll)(UserProfileContent);
+export default UserProfileContent;
